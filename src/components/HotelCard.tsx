204deb4945e8c687791c04206adb75e27a6e6b0b
Bossppp <peerawit_boss@hotmail.com>
2025-04-05 11:55:17 +0700

    feat: add hotelCard review detail and Icon

--- src/components/HotelCard.tsx
+++ src/components/HotelCard.tsx
@@ -1,22 +1,32 @@
+import { MapPin, Phone, Star } from 'lucide-react'
 import React from 'react'
 
 export default function HotelCard() {
   return (
     <div className='w-1/5 h-full bg-blue-500 rounded-lg'>
-      <div className="relative rounded-t-lg h-44 bg-gray-600">
+      <div className="rounded-t-lg h-44 bg-gray-600">
         <img
           src={"/img/hotel.jpg"}
           alt="hotel"
-          className="w-full h-full object-cover"
-        /> 
+          className="w-full h-full object-cover rounded-t-lg"
+        />
       </div>
-      <div className="p-4 h-56">
+      <div className="p-4 h-56 relative">
+        <div className="absolute top-2 right-2 px-2 py-1 flex items-center">
+          <Star className="text-black" size={16} />
+          <span className="ml-1 font-semibold text-gray-800">4.5</span>
+        </div>
+        <div className="absolute bottom-2 left-2 px-2 py-1">
+          <span className="text-sm font-medium text-gray-800">124 reviews</span>
+        </div>
         <h2 className="text-2xl font-semibold mb-2">name</h2>
         <div className="flex items-center mb-2">
-          <span className="ml-2">address</span>
+          <MapPin />
+          <span className="ml-2 font-medium">address</span>
         </div>
         <div className="flex items-center">
-          <span className="ml-2">tel</span>
+          <Phone />
+          <span className="ml-2 font-medium">tel</span>
         </div>
       </div>
     </div>
