81ade29352d24946afdeabf3569c966195883b10
Bossppp <peerawit_boss@hotmail.com>
2025-04-07 11:28:59 +0700

    feat: add function for edit delete to review and reply

--- src/components/ReplyDropDown.tsx
+++ src/components/ReplyDropDown.tsx
@@ -6,18 +6,34 @@ import {
   DropdownMenuTrigger,
 } from './ui/dropdown-menu';
 
-export default function ReviewDropDown() {
+interface ReplyDropDownProps {
+  replyId: number;
+  onEditReply: (replyId: number) => void;
+  onDeleteReply: (replyId: number) => void;
+}
+
+export default function ReplyDropDown({
+  replyId,
+  onEditReply,
+  onDeleteReply,
+}: ReplyDropDownProps) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>...</DropdownMenuTrigger>
       <DropdownMenuContent className='w-56' align='start'>
-        <DropdownMenuItem className='cursor-pointer'>
+        <DropdownMenuItem
+          className='cursor-pointer'
+          onClick={() => onEditReply(replyId)}
+        >
           <PencilLine color='#00a0f0' />
-          Edit
+          <span className='ml-2'>Edit</span>
         </DropdownMenuItem>
-        <DropdownMenuItem className='cursor-pointer'>
+        <DropdownMenuItem
+          className='cursor-pointer'
+          onClick={() => onDeleteReply(replyId)}
+        >
           <Trash color='red' />
-          Delete
+          <span className='ml-2'>Delete</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
