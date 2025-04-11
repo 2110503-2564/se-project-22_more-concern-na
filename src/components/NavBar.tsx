0f44184853aa685b3e45e043d15a8d055138cc2b
Palm •-• <ch.enderpalm@gmail.com>
2025-04-12 01:57:44 +0700

    feat: add functional navbar, all components (not linked)

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,26 +1,60 @@
+import { Hotel, LogOut, Settings, Wrench } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { Button } from './ui/button';
 
 function UserBar() {
-  return <>UserBar</>;
+  return (
+    <div className='flex items-center gap-2 text-white font-number'>
+      <div className='flex items-center gap-2'>
+        <span className='border-2 border-white rounded-full px-2'>P</span>
+        <span>150</span>
+      </div>
+      <Link href={'/logout'}>
+        <LogOut />
+      </Link>
+    </div>
+  );
 }
 
-function DashboardButton({ children }: { children: React.ReactNode }) {
-  return <div className='flex'>{children}</div>;
+function DashboardButton({
+  children,
+  link,
+}: {
+  children: React.ReactNode;
+  link: string;
+}) {
+  return (
+    <Link
+      href={link}
+      className='flex items-center gap-2 text-white font-heading hover:text-gold-gd1'
+    >
+      {children}
+    </Link>
+  );
 }
 
 export default function NavBar() {
-  const isLoggedIn = false;
+  const isLoggedIn = true;
   let role: 'admin' | 'hotelManager' | 'user' | null;
   role = 'admin';
   return (
-    <nav className='h-16 w-full flex items-center px-2'>
+    <nav className='h-16 w-full flex items-center px-4'>
       {role === 'admin' ? (
-        <DashboardButton>Admin</DashboardButton>
+        <DashboardButton link='/admin/report'>
+          <Settings />
+          Manage
+        </DashboardButton>
       ) : role === 'hotelManager' ? (
-        <DashboardButton>Hotel Manager</DashboardButton>
+        <DashboardButton link='/hotels'>
+          <Wrench />
+          Dashboard
+        </DashboardButton>
       ) : (
-        <DashboardButton>User</DashboardButton>
+        <DashboardButton link='/hotels'>
+          <Hotel />
+          View Hotels
+        </DashboardButton>
       )}
       <div className='h-full flex items-center justify-center grow'>
         <Image src={'/mcn-text.png'} alt='MCN Logo' width={150} height={0} />
