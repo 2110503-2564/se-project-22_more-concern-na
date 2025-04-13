34a73eeb923597ec4c4bfdb2087b1d3f6e5517d4
wishshukorn <hirhunwish@gmail.com>
2025-04-13 14:23:53 +0700

    feat(manage/hotels/page.tsx): add hotelCard

---
+++ src/app/manage/hotels/page.tsx
@@ -0,0 +1,56 @@
+'use client';
+import HotelCard from '@/components/HotelCard';
+import Loader from '@/components/Loader';
+import { getHotels } from '@/lib/hotelService';
+import { useEffect, useState } from 'react';
+import { HotelResponse, IHotel } from '../../../../interface';
+
+export default function Hotels() {
+  const [hotels, setHotels] = useState<HotelResponse>({
+    success: false,
+    count: 0,
+    data: [],
+    pagination: {
+      next: { page: 0, limit: 0 },
+      prev: { page: 0, limit: 0 },
+    },
+  });
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchHotels = async () => {
+      setIsLoading(true);
+      const responseData = await getHotels();
+      setHotels(responseData);
+      setIsLoading(false);
+    };
+    fetchHotels();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <Loader />
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className='
+        grid 
+        grid-cols-1 
+        sm:grid-cols-2 
+        md:grid-cols-3 
+        gap-6 
+        p-4
+        place-items-center
+      '
+    >
+      {hotels.data.map((hotel: IHotel, index) => (
+        <HotelCard key={index} type='edit' hotel={hotel} />
+      ))}
+    </div>
+  );
+}
